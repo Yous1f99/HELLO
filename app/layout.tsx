b6f9d89e5f86b07998e6e7d6009db4b69ce1d292
Yous1f99 <yousifmaher2016@gmail.com>
2025-07-11 10:22:02 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'YOUSIF Coffee',
+  description: 'Yous1f99',
+  generator: 'hi',
 }
 
 export default function RootLayout({
